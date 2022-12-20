400e41895bea3008cde6b8b513576a901efb6ad0
eteka03 <eteka03.akpaki@gmail.com>
2022-12-20 15:54:21 -0500

    add tailwind and start layout

---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-9xl'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ components/Layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-9xl'>
+        navbar
+    </div>
+  )
+}
+
+export default Navbar
